db2ebeb2a074fc44c2b21e4b7faa1b94d9a88f82
JackPytlik <jlpyt1@gmail.com>
2025-07-31 23:22:53 -0700

    Update tutoring.js

--- tutoring.js
+++ tutoring.js
@@ -19,7 +19,33 @@ window.addEventListener("scroll", () => {
     });
 
 if(/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-
+    document.getElementById("topbar").classList.add("mobile");
+    document.getElementsByClassName("title").classList.add("mobile");
+    document.getElementsByClassName("spacer").classList.add("mobile");
+    document.getElementById("tagline").classList.add("mobile");
+    document.getElementById("contactbox").classList.add("mobile");
+    document.getElementById("contacttitle").classList.add("mobile");
+    document.getElementById("contacttext").classList.add("mobile");
+    document.getElementsByClassName("heading").classList.add("mobile");
+    document.getElementsByClassName("bodytext").classList.add("mobile");
+    document.querySelectorAll("li").forEach(item => {
+        item.classList.add("mobile");
+    });
+    document.getElementsByClassName("leftcontain").classList.add("mobile");
+    document.getElementsByClassName("bottompad").classList.add("mobile");
 } else {
-
+    document.getElementById("topbar").classList.remove("mobile");
+    document.getElementsByClassName("title").classList.remove("mobile");
+    document.getElementsByClassName("spacer").classList.remove("mobile");
+    document.getElementById("tagline").classList.remove("mobile");
+    document.getElementById("contactbox").classList.remove("mobile");
+    document.getElementById("contacttitle").classList.remove("mobile");
+    document.getElementById("contacttext").classList.remove("mobile");
+    document.getElementsByClassName("heading").classList.remove("mobile");
+    document.getElementsByClassName("bodytext").classList.remove("mobile");
+    document.querySelectorAll("li").forEach(item => {
+        item.classList.remove("mobile");
+    });
+    document.getElementsByClassName("leftcontain").classList.remove("mobile");
+    document.getElementsByClassName("bottompad").classList.remove("mobile");
 }
